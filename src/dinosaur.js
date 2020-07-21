40923fdecbdfbf3a7a99db016315185b3e6bc247
Alissa <alissacrane123@gmail.com>
2020-07-21 11:48:58 -0700

    refactor to move()

--- src/dinosaur.js
+++ src/dinosaur.js
@@ -3,9 +3,9 @@ const Score = require('./score');
 class Dinosaur {
   constructor(board) {
     this.row = 8;
-    this.col = 10;
-    this.yPos = this.getPxPosition(8);
-    this.xPos = this.getPxPosition(10);
+    this.col = 17;
+    this.yPos = this.getPxPosition(this.row);
+    this.xPos = this.getPxPosition(this.col);
     this.xDir = 1;
     this.yDir = 0;
     this.dx = (window.innerWidth * 3) / 100;
@@ -28,13 +28,13 @@ class Dinosaur {
     this.collectCoin = this.collectCoin.bind(this);
     this.move = this.move.bind(this);
 
-    this.timer = setTimeout(this.move, 100);
+    this.timer = setTimeout(this.move, 500);
 
     // this.setSpan();
     this.createDino();
     this.registerListeners();
     this.startAnimation();
-    this.startMoving();
+    // this.startMoving();
 
     console.log(this.dx, this.dy);
   }
@@ -113,8 +113,8 @@ class Dinosaur {
 
   startMoving() {
     this.posInterval = setInterval(() => {
-      if (this.moving) {
-        this.updatePos(this.xDir, this.yDir);
+      if (this.moving && !this.isCollision()) {
+        this.updateRowAndCol(this.xDir, this.yDir);
       }
     }, 500);
   }
@@ -125,28 +125,84 @@ class Dinosaur {
     this.osx = Math.abs((this.osx + 1) % 4 * this.xDir);
     this.osy = Math.abs((this.osy + 1) % 4 * this.yDir);
 
-    if (this.osx == 0 && this.osy === 0) {
-      
+    if (!this.isCollision()) {
+
+      this.xPos = this.xPos + this.xDir * this.step;
+      this.yPos = this.yPos + this.yDir * this.step;
+      // debugger
+      if (this.osx == 0 && this.osy === 0) {
+        this.updateRowAndCol();
+      } else {
+        this.moveDino();
+      }
     }
 
-    this.timer = setTimeout(this.move, 100);
+
+    this.timer = setTimeout(this.move, 500);
   }
 
-  updatePos() {
-    if (!this.isCollision()) {
+  is180(xDir, yDir) {
+    if ((xDir === 1 && this.xDir === -1) ||
+        (xDir === -1 && this.xDir === 1)) {
+      return true;
+    }
+    if ((yDir === 1 && this.yDir === -1) ||
+        (yDir === -1 && this.yDir === 1)) {
+      return true;
+    }
+
+    return false;
+  }
+
+  setRowAndColPos() {
+
+    // if (!this.isCollision() && !this.is180()) {
+    //   this.updateRowAndCol();
+    //   this.xPos = this.getPxPosition(this.col);
+    //   this.yPos = this.getPxPosition(this.row);
+    //   this.osx = 0;
+    //   this.osy = 0;
+    // } else if (this.is180()) {
+    //   this.osx = 4 - this.osx;
+    //   this.osy = 4 - this.osy;
+    // } else {
+    //   this.osx = 0;
+    //   this.osy = 0;
+    // }
+    if (!this.isCollision() && (this.osx > 2 || this.osy > 2)) {
+      // && (this.osx > 2 || this.osy > 2)
+      this.updateRowAndCol();
+    } 
+    this.osx = 0;
+    this.osy = 0;
+
+
+    this.xPos = this.getPxPosition(this.col);
+    this.yPos = this.getPxPosition(this.row);
+    this.moveDino();
+  }
+
+  updateRowAndCol() {
+    // if (!this.isCollision()) {
       this.row += this.yDir;
       this.col += this.xDir;
-      this.xPos = this.xPos + this.xDir * this.dx;
-      this.yPos = this.yPos + this.yDir * this.dy;
+      // this.xPos = this.xPos + this.xDir * this.step;
+      // this.yPos = this.yPos + this.yDir * this.step;
+      // this.xPos = this.xPos + this.xDir * this.dx;
+      // this.yPos = this.yPos + this.yDir * this.dy;
       this.moveDino();
       this.collectCoin();
-    }
+    // }
   }
 
   updateDir(dir, xDir, yDir) {
     this.dinoEl.classList.remove(this.dir);
     this.dinoEl.classList.add(dir);
     this.dir = dir;
+    
+    if (this.xDir !== xDir || this.yDir !== yDir) {
+      this.setRowAndColPos(xDir, yDir);
+    }
     this.xDir = xDir;
     this.yDir = yDir;
   }
@@ -159,6 +215,8 @@ class Dinosaur {
   isCollision() {
     let newRow = this.row + this.yDir;
     let newCol = this.col + this.xDir;
+
+
     let tileId = `row${newRow}-col${newCol}`;
     let tile = document.getElementById(tileId);
 
